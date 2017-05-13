bc4215d442a468b74fb020ea35c157e5f3eb940c
Joe Maples <joe@frap129.org>
2017-05-12 21:08:01 -0500

    drivers/cpufreq: importing electron governer

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -582,6 +582,10 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
+extern struct cpufreq_governor cpufreq_gov_electron;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electron)
+
 #endif
 
 /*********************************************************************
